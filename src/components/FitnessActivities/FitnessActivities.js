ec21b6f47a135311a5645b687e1a947bec049816
sazzadhossainapple <sazzadapple81@gmail.com>
2022-09-28 22:45:32 +0600

    api data load

--- src/components/FitnessActivities/FitnessActivities.js
+++ src/components/FitnessActivities/FitnessActivities.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FitnessActivities = () => {
+  const [fitnessActivities, setFitnessActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("gymFitness.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <h1>The Fitness & Body Club</h1>
+      <div></div>
     </div>
   );
 };
