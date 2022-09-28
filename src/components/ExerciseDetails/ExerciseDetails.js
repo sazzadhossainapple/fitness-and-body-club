9f47f1856c2a8803e3027677c6dacd0708e27cf3
sazzadhossainapple <sazzadapple81@gmail.com>
2022-09-29 05:00:25 +0600

    local storage data save and get

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BreakTime from "../BreakTime/BreakTime";
 import UserDetails from "../UserDetails/UserDetails";
 
 const ExerciseDetails = ({ exerciseTime }) => {
   const [breakTime, setBreakTime] = useState(0);
+  const [getStoreData, setGetStoreData] = useState(0);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("break-time"));
+    if (data) {
+      setGetStoreData(data);
+    }
+  }, [breakTime]);
 
   // total time calculate
   const totalTimeRequired = exerciseTime.reduce(
@@ -13,6 +21,7 @@ const ExerciseDetails = ({ exerciseTime }) => {
 
   const handleBreakTime = (event) => {
     setBreakTime(event.target.value);
+    localStorage.setItem("break-time", JSON.stringify(event.target.value));
   };
 
   return (
@@ -27,7 +36,7 @@ const ExerciseDetails = ({ exerciseTime }) => {
         </div>
         <div className="mt-6 bg-slate-50 p-4 rounded-md flex justify-between">
           <h2 className="text-base font-medium">Break time</h2>
-          <p>{breakTime} seconds</p>
+          <p>{getStoreData} seconds</p>
         </div>
       </div>
       <div className="my-4">
